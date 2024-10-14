8cf6598f178f4cad2c963c80807e2001d77ead3d
paolasoria <paola.soria0597@gmail.com>
2024-10-14 19:54:35 -0400

    added controlled forms using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-// src/App.jsx
+import Bookshelf from './Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+  <h1>Hello world!</h1>;
+  <Bookshelf />
+  </>
+  );
 };
 
 export default App;
